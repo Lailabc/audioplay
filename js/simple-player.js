75079482de8e510a5f1282517e6a0da28698157c
Lailabc <115160711+Lailabc@users.noreply.github.com>
2022-11-08 20:08:46 +0000

    Add files via upload

--- js/simple-player.js
+++ js/simple-player.js
@@ -1,13 +1,45 @@
 const audio = new Audio();
+const img = new Image ();
 const button = document.getElementById("play-pause-button");
 const trackTime = document.getElementById("current-time");
 const totalTime = document.getElementById("total-time");
 const seekBar = document.getElementById("seek-bar");
+const showImg = document.getElementById("img-container");
 let seeking = false;
 const track1Button = document.getElementById("track1");
 track1Button.onclick = function() {
     audio.src="audio/track1.webm"
-}
+};
+const track2Button = document.getElementById("track2");
+track2Button.onclick = function() {
+    audio.src="audio/track2.webm"
+};
+const track3Button = document.getElementById("track3");
+track3Button.onclick = function() {
+    audio.src="audio/track3.webm"
+};
+/*trying to show image by clicking on the track button
+const track1ButtonImg = document.getElementById("imgtrack1");
+track1ButtonImg.onclick = function() {
+    img.src="img/sunrise.gif"
+};
+
+const track2ButtonImg = document.getElementById("imgtrack2");
+track2ButtonImg.onclick = function() {
+    img.src="img/sunset.gif"
+};
+
+const track3ButtonImg = document.getElementById("imgtrack3");
+track3ButtonImg.onclick = function() {
+    img.src="img/twilight.gif"
+};
+showImg.onclick = function () {
+    if(track1Button){
+        track1ButtonImg
+    } else {
+       console.log("choose another track");
+}*/
+
 button.onclick = function () {
     if (audio.paused) {
         audio.play();
@@ -72,3 +104,4 @@ function formatTime(secs) {
         return minutes + ":" + seconds;
     }
 }
+
